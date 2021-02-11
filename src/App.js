a90a1588ac53a83b8ed12259e279a4885ea43f00
Ashley Vermaas <ashleyvermaas1@gmail.com>
2021-02-11 12:32:11 +0100

    complete with codealong

--- src/App.js
+++ src/App.js
@@ -1,23 +1,35 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 import Navbar from './components/Navbar';
 import CountriesList from './components/CountriesList';
 import CountryDetails from './components/CountryDetails';
+import countries from './countries.json';
+import { Link, Switch, Route } from 'react-router-dom';
 
-function App() {
-  return (
-    <div className="App">
-      <Navbar />
+class App extends React.Component {
+  state = {
+    countries: countries
+  }
 
-      <div className="container">
-        <div className="row">
-          <CountriesList />
-          <CountryDetails />
+  render() {
+    return (
+      <div className="App">
+        <Navbar />
+
+        <div className="container">
+          <div className="row">
+            <CountriesList countriesList={this.state.countries} />
+
+            <Switch>
+            <Route path='/:countryCode' render={props => {
+              return <CountryDetails {...props} countriesList={this.state.countries} />
+            }} />
+            </Switch>
+          </div>
         </div>
       </div>
-    </div>
-  );
+    );
+  }
 }
 
 export default App;
